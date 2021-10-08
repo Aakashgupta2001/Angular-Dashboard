1b5fcee236dc9c635f8cb8d5ff2a57de2bb80c21
sakshampatil <69973154+sakshampatil@users.noreply.github.com>
2021-10-08 20:34:39 +0530

    dashboard routing done

--- dashboard/src/app/app-routing.module.ts
+++ dashboard/src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CodeComponent } from './components/code/code.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { SyllabusComponent } from './components/syllabus/syllabus.component';
+import { VideosComponent } from './components/videos/videos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'syllabus', component: SyllabusComponent },
+  { path: 'schedule', component: ScheduleComponent },
+  { path: 'notes', component: NotesComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: 'code', component: CodeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+export const routingComponents = [
+  HomeComponent,
+  SyllabusComponent,
+  ScheduleComponent,
+  NotesComponent,
+  VideosComponent,
+  CodeComponent,
+];
--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { IgxIconModule, IgxNavbarModule } from 'igniteui-angular';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from './components/button/button.component';
-import { HomeComponent } from './components/home/home.component';
-import { SyllabusComponent } from './components/syllabus/syllabus.component';
-import { ScheduleComponent } from './components/schedule/schedule.component';
-import { NotesComponent } from './components/notes/notes.component';
-import { CodeComponent } from './components/code/code.component';
-import { VideosComponent } from './components/videos/videos.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, NavbarComponent, ButtonComponent, HomeComponent, SyllabusComponent, ScheduleComponent, NotesComponent, CodeComponent, VideosComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    ButtonComponent,
+    routingComponents,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
