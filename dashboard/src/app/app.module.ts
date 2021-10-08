1781ea55fa8972192a033f15de8de6d4c05e2f6b
sakshampatil <69973154+sakshampatil@users.noreply.github.com>
2021-10-08 18:03:22 +0530

    dashboard components

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -9,9 +9,15 @@ import { IgxIconModule, IgxNavbarModule } from 'igniteui-angular';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from './components/button/button.component';
+import { HomeComponent } from './components/home/home.component';
+import { SyllabusComponent } from './components/syllabus/syllabus.component';
+import { ScheduleComponent } from './components/schedule/schedule.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { CodeComponent } from './components/code/code.component';
+import { VideosComponent } from './components/videos/videos.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, NavbarComponent, ButtonComponent],
+  declarations: [AppComponent, DashboardComponent, NavbarComponent, ButtonComponent, HomeComponent, SyllabusComponent, ScheduleComponent, NotesComponent, CodeComponent, VideosComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
