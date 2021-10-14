fea8c369a825b6e69c54ffae91bdce7c400277f8
sakshampatil <69973154+sakshampatil@users.noreply.github.com>
2021-10-15 01:02:57 +0530

    post requested added

--- dashboard/src/app/components/signup/signup.component.ts
+++ dashboard/src/app/components/signup/signup.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DataFetchService } from 'src/app/services/dataFetch/data-fetch.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  json;
+  constructor(private user: DataFetchService) {
+    this.user
+      .postUserData()
+      .toPromise()
+      .then((data: any) => {
+        console.log(data);
+        this.json = data.json;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
+  ngOnInit(): void {}
 }
