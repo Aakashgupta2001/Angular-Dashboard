f3548d2f12cb7c2b929cacfada75dcbc22b32728
Aakash Gupta <aakashgupta2001.2909@gmail.com>
2021-10-16 12:20:40 +0530

    verify token

--- app/controller/user.controller.js
+++ app/controller/user.controller.js
@@ -54,3 +54,24 @@ exports.login = async (req, res) => {
     });
   } catch (err) {}
 };
+
+module.exports.verifyToken = function (req, res, next) {
+  //get the token from the header if present
+  token = req.headers.authorization;
+  //if no token found, return response (without going to the next middelware)
+  if (!token) {
+    return res.status(400).send("Unauthorzied asd");
+  }
+  try {
+    if (token.includes("Bearer")) {
+      token = token.substr(7);
+    }
+    //if can verify the token, set req.user and pass to next middleware
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = decoded;
+    return res.status(200).send(decoded);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Invalid Token");
+  }
+};
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -3,5 +3,6 @@ const userController = require("../controller/user.controller");
 
 router.route("/login").post(userController.login);
 router.route("/signup").post(userController.signup);
+router.route("/verify").post(userController.verifyToken);
 
 module.exports = router;
